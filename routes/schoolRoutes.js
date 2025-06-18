6fba28ec18a3264ee067c55d8bb9546f42358c29
laladwesh <guptaavinash302@gmail.com>
2025-06-19 00:45:06 +0530

    school routes added changes

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -3,11 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const schoolController = require("../controllers/schoolController");
-const { authenticate, isAdmin , superAdminAuth } = require("../middleware/authMiddleware");
+const { authenticate, isAdmin , superAdminAuth, isSuperAdminAuth } = require("../middleware/authMiddleware");
 // ← You’ll only need protect/authorize if you want to restrict who can create/list schools.
 // For instance, maybe only a super‐admin can create a School.
 
-router.get("/", schoolController.listSchools);
+router.get("/", authenticate ,schoolController.listSchools);
+router.get('/:id',    authenticate, isSuperAdminAuth ,schoolController.getSchoolWithFullDetails);
 
 router.post(
   "/",
