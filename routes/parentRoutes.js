024797629e1779bc5b795ebfeeb7fd65b28b2bac
laladwesh <guptaavinash302@gmail.com>
2025-06-20 18:29:59 +0530

    cahnges

--- routes/parentRoutes.js
+++ routes/parentRoutes.js
@@ -1,30 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
+function superAdminoradminAuth(req, res, next) {
+  const { role } = req.user;
+  if (role === "super_admin" || role === "school_admin") {
+    return next();
+  }
+  return res
+    .status(403)
+    .json({ success: false, message: "Not authorized to access this resource." });
+}
+
+
 // Middleware to verify authentication and admin role
 const {
   authenticate,
-  isAdmin,
-  isSuperAdminAuth,
 } = require("../middleware/authMiddleware");
 
 router.get(
   "/:id",
   authenticate,
-  isSuperAdminAuth,
+  superAdminoradminAuth,
   require("../controllers/parentController").getParentWithFullDetails
 );
 router.put(
   "/:id",
   authenticate,
-  isSuperAdminAuth,
+   superAdminoradminAuth,
   require("../controllers/parentController").updateParent
 );
 
 router.delete(
   "/:id",
   authenticate,
-  isSuperAdminAuth,
+  superAdminoradminAuth,
   require("../controllers/parentController").deleteParent
 );
 
